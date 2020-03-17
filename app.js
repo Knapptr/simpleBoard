1ff0e6e72940fa9ba1b96017af66a6eac4d4d7a7
Tyler Knapp <knapptr@gmail.com>
2020-03-16 21:03:10 -0700

    add bin for heroku deployment

--- app.js
+++ app.js
@@ -11,7 +11,4 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', indexRouter);
 
-app.listen(3000, () => {
-    console.log('listening on 3000!')
-})
-
+module.exports = app;
